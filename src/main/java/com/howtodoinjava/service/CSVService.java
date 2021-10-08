e06fc10e9ad79ae2d1f71ee7af5d24e8936c7200
apundir321 <65084641+apundir321@users.noreply.github.com>
2021-10-08 17:26:42 +0530

    Update CSVService.java

--- src/main/java/com/howtodoinjava/service/CSVService.java
+++ src/main/java/com/howtodoinjava/service/CSVService.java
@@ -8,6 +8,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
@@ -30,9 +31,12 @@ import com.howtodoinjava.domain.JobAccountCustomRepo;
 import com.howtodoinjava.domain.JobAccountRepo;
 import com.howtodoinjava.domain.JobTypeRepo;
 import com.howtodoinjava.domain.TimeSlotRepo;
+import com.howtodoinjava.entity.Category;
+import com.howtodoinjava.entity.DayPreference;
 import com.howtodoinjava.entity.JobAccount;
 import com.howtodoinjava.entity.JobAccountApplication;
 import com.howtodoinjava.model.JobApplication;
+import com.howtodoinjava.model.JobCategory;
 import com.howtodoinjava.model.User;
 import com.howtodoinjava.model.UserProfile;
 import com.howtodoinjava.security.UserService;
@@ -40,10 +44,10 @@ import com.howtodoinjava.security.UserService;
 @Service
 public class CSVService {
 
-  @Autowired
-  JobAccountRepo repository;
-  
-  @Autowired
+	@Autowired
+	JobAccountRepo repository;
+
+	@Autowired
 	CategoryRepo categoryRepo;
 
 	@Autowired
@@ -66,188 +70,185 @@ public class CSVService {
 
 	@Autowired
 	UserProfileService userService;
-	
-	
+
 	@Autowired
 	JobEarningRepo jobEarningRepo;
-	
-	
+
 	@Autowired
 	UserService uService;
 
 	@Autowired
 	UserProfileRepository userProfileRepo;
-	
+
 	@Autowired
 	JobAccountApplicationRepo jobAccountApplicationRepo;
-	
+
 	@Autowired
 	DayPreferenceRepo dayPreferenceRepo;
-	
+
 	@Autowired
 	JobAccountCustomRepo jobAccountCustomRepo;
-  
-  public ByteArrayInputStream load() {
-    List<JobAccount> tutorials = repository.findAll();
 
-    ByteArrayInputStream in = jobsToCSV(tutorials);
-    return in;
-  }
-  
-  public ByteArrayInputStream loadProfileData() {
-		Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+	public ByteArrayInputStream load() {
+		List<JobAccount> tutorials = repository.findAll();
+
+		ByteArrayInputStream in = jobsToCSV(tutorials);
+		return in;
+	}
+
+	public ByteArrayInputStream loadProfileData() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User user = userRepo.findByEmail(authentication.getName());
 
-	    ByteArrayInputStream in = profileToCSV((user.getUserProfile()));
-	    return in;
-	  }
+		ByteArrayInputStream in = profileToCSV((user.getUserProfile()));
+		return in;
+	}
 
-  public ByteArrayInputStream loadAppliedJobsData() {
-	  Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+	public ByteArrayInputStream loadAppliedJobsData() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User user = userRepo.findByEmail(authentication.getName());
 		List<JobAccountApplication> applications = jobAccountApplicationRepo.findAllByApplicant(user);
-	    ByteArrayInputStream in = appliedJobsToCSV(applications);
-	    
-	    
-	    return in;
-	  }
-  
-  public ByteArrayInputStream loadProfilesData() {
-	  Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+		ByteArrayInputStream in = appliedJobsToCSV(applications);
+
+		return in;
+	}
+
+	public ByteArrayInputStream loadProfilesData() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User user = userRepo.findByEmail(authentication.getName());
 		List<UserProfile> applications = (List<UserProfile>) userProfileRepo.findAll();
-	    ByteArrayInputStream in = profilesToCSV(applications);
-	    return in;
-	  }
-  
- 
-  
-  
-  public static ByteArrayInputStream jobsToCSV(List<JobAccount> jobAccounts) {
-	    final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
+		ByteArrayInputStream in = profilesToCSV(applications);
+		return in;
+	}
 
-	    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
-	    	DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd"); 
-	      for (JobAccount jobAccount : jobAccounts) {
-	        List<String> data = Arrays.asList(
-	             jobAccount.getJobCode(),
-	             jobAccount.getEmployer().getEmployerName(),
-	             jobAccount.getTimeSlot()==null?null:jobAccount.getTimeSlot().getTimeSlotName(),
-	             jobAccount.getCategory()==null?null:jobAccount.getCategory().getCategoryName(),
-	             String.valueOf(jobAccount.getNoOfVacancy()),
-	             jobAccount.getCity(),
-	             dateFormat.format(jobAccount.getJobDate()),
-	             jobAccount.getStatus()
-	            );
+	public static ByteArrayInputStream jobsToCSV(List<JobAccount> jobAccounts) {
+		final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
 
-	        csvPrinter.printRecord(data);
-	      }
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+				CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+			for (JobAccount jobAccount : jobAccounts) {
+				List<String> data = Arrays.asList(jobAccount.getJobCode(), jobAccount.getEmployer().getEmployerName(),
+						jobAccount.getTimeSlot() == null ? null : jobAccount.getTimeSlot().getTimeSlotName(),
+						jobAccount.getCategory() == null ? null : jobAccount.getCategory().getCategoryName(),
+						String.valueOf(jobAccount.getNoOfVacancy()), jobAccount.getCity(),
+						dateFormat.format(jobAccount.getJobDate()), jobAccount.getStatus());
 
-	      csvPrinter.flush();
-	      return new ByteArrayInputStream(out.toByteArray());
-	    } catch (IOException e) {
-	      throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
-	    }
-	  }
-  
-  public static ByteArrayInputStream profileToCSV(UserProfile userProfile) {
-	    final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
-	    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
-	    	DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd"); 
-	    	
-	    	 List<String> data = Arrays.asList(
-	    			 String.valueOf(userProfile.getId()),
-	    			 userProfile.getFirstName(),
-	    			 userProfile.getEmail(),
-	    			 userProfile.getOtherDetails().getMobileNo(),
-	    			 dateFormat.format(userProfile.getDob()),
-	    			 userProfile.getOtherDetails().getCourse(),
-	    			 userProfile.getOtherDetails().getCity(),
-	    			 userProfile.getOtherDetails().getState(),
-	    			 userProfile.getOtherDetails().getStatus()
-	    			 );
-	    	
-	    
+				csvPrinter.printRecord(data);
+			}
 
-	        csvPrinter.printRecord(data);
-	      
+			csvPrinter.flush();
+			return new ByteArrayInputStream(out.toByteArray());
+		} catch (IOException e) {
+			throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
+		}
+	}
 
-	      csvPrinter.flush();
-	      return new ByteArrayInputStream(out.toByteArray());
-	    } catch (IOException e) {
-	      throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
-	    }
-	  }
-  
+	public static ByteArrayInputStream profileToCSV(UserProfile userProfile) {
+		final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+				CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
 
-  public static ByteArrayInputStream profilesToCSV(List<UserProfile> userProfiles) {
-	    final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
-	    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
-	    	DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd"); 
-	    	for(UserProfile userProfile: userProfiles) {
-	    	 List<String> data = Arrays.asList(
-	    			 String.valueOf(userProfile.getId()),
-	    			 userProfile.getFirstName(),
-	    			 userProfile.getEmail(),
-	    			 userProfile.getGender(),
-	    			 userProfile.getParentsName(),
-	    			 userProfile.getBloodGroup(),
-	    			 userProfile.getDob()==null?"":dateFormat.format(userProfile.getDob()),
-	    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getMobileNo(),
-	    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getCourse(),
-	    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getCity(),
-	    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getState(),
-	    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getStatus(),
-	    					 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getVehicleType(),
-	    			    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getAddressLine1(),
-	    			    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getAddressLine2(),
-	    			    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getStreetNo(),
-	    			    					 userProfile.getOtherDetails()==null?"":String.valueOf(userProfile.getOtherDetails().isHavePc()),
-	    			    			    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getCollegeName(),
-	    			    			    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getLandmark(),
-	    			    			    			 userProfile.getOtherDetails()==null?"":userProfile.getOtherDetails().getLocality()
-	    			 
-	    			 );
-	    	
-	    
+			List<String> data = Arrays.asList(String.valueOf(userProfile.getId()), userProfile.getFirstName(),
+					userProfile.getEmail(), userProfile.getOtherDetails().getMobileNo(),
+					dateFormat.format(userProfile.getDob()), userProfile.getOtherDetails().getCourse(),
+					userProfile.getOtherDetails().getCity(), userProfile.getOtherDetails().getState(),
+					userProfile.getOtherDetails().getStatus());
 
-	        csvPrinter.printRecord(data);
-	      
-	    	}
-	      csvPrinter.flush();
-	      return new ByteArrayInputStream(out.toByteArray());
-	    } catch (Exception e) {
-	      throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
-	    }
-	  }
+			csvPrinter.printRecord(data);
 
+			csvPrinter.flush();
+			return new ByteArrayInputStream(out.toByteArray());
+		} catch (IOException e) {
+			throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
+		}
+	}
 
-  
-  
-  public static ByteArrayInputStream appliedJobsToCSV(List<JobAccountApplication> jobApplications) {
-	    final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
-	    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
-	    	DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd"); 
-	    	 for (JobAccountApplication jobApplication : jobApplications) {
-	    	 List<String> data = Arrays.asList(
-	    			 String.valueOf(jobApplication.getId()),
-	    			 jobApplication.getJob().getJobName(),
-	    			 jobApplication.getJob().getCategory().getCategoryName(),
-	    			 jobApplication.getJob().getTimeSlot()==null?null:jobApplication.getJob().getTimeSlot().getTimeSlotName(),
-	    			 jobApplication.getApplicant().getEmail(),
-	    			 jobApplication.getApplicant().getPhoneNo(),
-	    			 dateFormat.format(jobApplication.getApplicationDate())
-	    			 );
-	    	 csvPrinter.printRecord(data);
-	    	 }
-	      csvPrinter.flush();
-	      return new ByteArrayInputStream(out.toByteArray());
-	    } catch (IOException e) {
-	      throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
-	    }
-	  }
+	public static ByteArrayInputStream profilesToCSV(List<UserProfile> userProfiles) {
+		final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+				CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+			for (UserProfile userProfile : userProfiles) {
+
+				String jobCategories = "";
+				if (userProfile.getOtherDetails() != null) {
+					if (userProfile.getOtherDetails().getJobCategories() != null) {
+						List<Category> categories = userProfile.getOtherDetails().getJobCategories();
+						for (Category category : categories) {
+							jobCategories += category.getCategoryName() + ",";
+						}
+
+					}
+				}
+
+				String preferences = "";
+				if (userProfile.getOtherDetails() != null) {
+					if (userProfile.getOtherDetails().getPreferences() != null) {
+						Set<DayPreference> prefs = userProfile.getOtherDetails().getPreferences();
+						for (DayPreference pref : prefs) {
+							preferences += pref.getDay() + "=" + pref.getTimeSlot().getTimeSlotName() + ",";
+						}
+
+					}
+				}
+
+				List<String> data = Arrays.asList(String.valueOf(userProfile.getId()), userProfile.getFirstName(),
+						userProfile.getLastName(), userProfile.getEmail(), userProfile.getGender(),
+						userProfile.getParentsName(), userProfile.getBloodGroup(),
+						userProfile.getDob() == null ? "" : dateFormat.format(userProfile.getDob()),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getMobileNo(),
+						userProfile.getOtherDetails() == null ? ""
+								: userProfile.getOtherDetails().getAlternateMobileNo(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getCourse(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getCity(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getState(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getStatus(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getVehicleType(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getAddressLine1(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getAddressLine2(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getStreetNo(),
+						userProfile.getOtherDetails() == null ? ""
+								: String.valueOf(userProfile.getOtherDetails().isHavePc()),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getCollegeName(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getLandmark(),
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getLocality(),
+						userProfile.getOtherDetails() == null ? ""
+								: dateFormat.format(userProfile.getOtherDetails().getDegreeCollegeCompletionDate()),
+						jobCategories, preferences,
+						userProfile.getOtherDetails() == null ? "" : userProfile.getOtherDetails().getReferralCode()
+
+				);
+
+				csvPrinter.printRecord(data);
+
+			}
+			csvPrinter.flush();
+			return new ByteArrayInputStream(out.toByteArray());
+		} catch (Exception e) {
+			throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
+		}
+	}
+
+	public static ByteArrayInputStream appliedJobsToCSV(List<JobAccountApplication> jobApplications) {
+		final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+				CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+			for (JobAccountApplication jobApplication : jobApplications) {
+				List<String> data = Arrays.asList(String.valueOf(jobApplication.getId()),
+						jobApplication.getJob().getJobName(), jobApplication.getJob().getCategory().getCategoryName(),
+						jobApplication.getJob().getTimeSlot() == null ? null
+								: jobApplication.getJob().getTimeSlot().getTimeSlotName(),
+						jobApplication.getApplicant().getEmail(), jobApplication.getApplicant().getPhoneNo(),
+						dateFormat.format(jobApplication.getApplicationDate()));
+				csvPrinter.printRecord(data);
+			}
+			csvPrinter.flush();
+			return new ByteArrayInputStream(out.toByteArray());
+		} catch (IOException e) {
+			throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
+		}
+	}
 }
